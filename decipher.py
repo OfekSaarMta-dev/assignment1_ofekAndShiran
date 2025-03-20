857e9ae15c2dfeb54e0e6cbe96e7dc287eb41afd
Shiran Belachdev <shiranbel56@gmail.com>
2025-03-20 14:57:36 +0200

    third commit

--- decipher.py
+++ decipher.py
@@ -1,21 +1,26 @@
 import json
 import pickle
 
+def load_lexicon(filename):
+    """Loads the lexicon as a set of words from a file."""
+    if not os.path.exists(filename):
+        print(f"Error: The file '{filename}' does not exist.")
+        return None
+    with open(filename, 'rb', encoding='utf8') as file:
+        return set(word.strip() for word in file.readlines())
+
 
 def decipher_phrase(phrase, lexicon_filename, abc_filename):
     # todo: implement this function
     print(f'starting deciphering using {lexicon_filename} and {abc_filename}')
 
-    # todo: due to possible difference in file encodings between operating systems, make sure to add
-    #  utf8 encoding type when opening a file, as an example: with open(<file name>, 'r', encoding='utf8') as fin
-    #  python developers plan to make utf8 a default at 3.15 - https://peps.python.org/pep-0686/
-
-    result = {"status": 0, "orig_phrase": '', "K": -1}
+    result = {"status": 1, "orig_phrase": '', "K": -1} # if we cant decipher what should we return !!!!!!!!
     return result
 
 
+
 # todo: fill in your student ids
-students = {'id1': '000000000', 'id2': '000000000'}
+students = {'id1': '322756586', 'id2': '211449145'}
 
 if __name__ == '__main__':
     with open('config-decipher.json', 'r') as json_file:
