651657fbac72f90ce914259dd815f67f9083b807
Shiran Belachdev <shiranbel56@gmail.com>
2025-03-20 21:18:57 +0200

    Changes by User

--- decipher.py
+++ decipher.py
@@ -1,5 +1,10 @@
 import json
 import pickle
+import os
+from xmlrpc.server import resolve_dotted_attribute
+
+def letter_to_number(letter):
+        return ord(letter) - ord('a') + 1
 
 def load_lexicon(filename):
     """Loads the lexicon as a set of words from a file."""
@@ -11,7 +16,31 @@ def load_lexicon(filename):
 
 
 def decipher_phrase(phrase, lexicon_filename, abc_filename):
-    # todo: implement this function
+    if not os.path.exists(abc_filename):
+        print(f"Error: The file '{abc_filename}' does not exist.")
+        return None  # Exit gracefully
+
+    words_list = phrase.split()
+    split_words = [list(word) for word in words_list]
+
+    with open(abc_filename, 'r') as abc_filename:
+        abc_dict = dict()
+        # create
+        for k in range (1, 27):
+            abc_dict[k]= abc_filename.readline().strip()
+
+
+        for word in split_words:
+            new_word = ""  # Initialize an empty string for each word
+            for k in range (26):
+                new_words = []
+                for letter in word:
+                    new_letter = abc_dict[(letter_to_number(letter) - k - 1) % 26 + 1]
+                    new_word +=  new_letter # Append transformed letter
+                new_words.append(new_word)
+
+
+
     print(f'starting deciphering using {lexicon_filename} and {abc_filename}')
 
     result = {"status": 1, "orig_phrase": '', "K": -1} # if we cant decipher what should we return !!!!!!!!
